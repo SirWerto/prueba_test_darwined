f5113ec11fc8e922b5a825f33784b9b23a9020ab
SirWerto <sirwerto@gmail.com>
2021-01-13 17:52:05 +0100

    Primera propiedad

---
+++ test_asignatura.py
@@ -0,0 +1,69 @@
+import pandas as pd
+
+
+
+##################
+### Generators ###
+##################
+
+def gen_val_prio(LenSalas):
+    yield 0
+    Counter = 0
+    i = 1
+    while i < LenSalas+1:
+        Counter += i
+        i += 1
+        yield Counter
+
+########################   
+### Check Properties ###
+########################
+
+def propiedad_online_numerospermitidos(row):
+    if row["ONLINE"] not in [0,1]:
+        Tupla = ("asignaturas", str(row["ClaveReporte"]), "ONLINE", "error", "Valor no permitido", "Valores permitidos [0, 1]")
+        return Tupla
+    else:
+        return None
+
+
+######################
+### Test Functions ###
+######################
+
+
+############################    
+### Private Functions ###
+############################
+
+def crear_clave(row):
+    Clave = str(x["SEDE"])+str(x["ESCUELA"])+str(x["MODALIDAD"])+str(x["JORNADA"])+str(x["CARRERA"])+str(x["CURRICULO"])+str(x["ASIGNATURA"])+str(x["COMPONENTE"])
+    return Clave
+
+def apply_row(df, func, columns, **kargs):
+    columns.append("ClaveReporte")
+    tofix = df[columns].apply(func, axis=1, **kargs).dropna().values.tolist()
+    return tofix
+
+############################    
+### Catalogue Validation ###
+############################
+
+
+def validacion_asignatura(Asignaturas, TSalas, Franjas):
+
+    Asignaturas["ClaveReporte"] = Asignaturas.apply(crear_clave, axis=1)
+    Asignaturas.loc[:,"Aviso"] = ""
+
+    report = []
+
+    report += apply_row(Asignaturas, propiedad_online_numerospermitidos, ["ONLINE"])
+
+    if TSalas != None:
+        print("hola")
+    if Franjas != None:
+        print("hola")
+
+    return report
+
+    
